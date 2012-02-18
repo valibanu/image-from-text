56231f6a79d57a9a3457fabab677733088097526
Valerian Banu <vali.banu@gmail.com>
2012-02-18 17:50:38 +0200

    Added main

--- main.py
+++ main.py
@@ -3,9 +3,22 @@ Created on Feb 18, 2012
 
 @author: Vali
 '''
+import sys
+
 from bing.bingimagesearch import BingImageSearch
+from extractor.clean import clean_input, get_paragraphs
+from extractor.ner import ne_preprocess
+
 
 if __name__ == '__main__':
-    bing = BingImageSearch() 
-    for pic in bing.search('green apples'):
-        print(pic)
+    input = sys.argv[1]
+#    input = u"Every story;;http://www.3applesbookaward.org/yr/promote/images/GreenApple.jpg;;http://doh.sd.gov/diabetes/img/GreenApples.jpg;;http://www.wallpapers.manbiz.eu/wallpapers/green_apples-1280x800.jpg"
+    input = clean_input(input)
+#    print(input)
+    
+    bing = BingImageSearch()    
+    for paragraph in get_paragraphs(input):
+        keywords = ne_preprocess(paragraph)
+        picture_urls = [pic.url for pic in bing.search(' '.join(keywords), 3)]
+        print('{0};;{1}'.format(paragraph,
+                                ';;'.join(picture_urls)))
