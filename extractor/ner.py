48081e9e95eaa98cb626cd53988f8c470d2e5bd8
Mihai Bivol <mm.bivol@gmail.com>
2012-02-18 17:50:53 +0200

    Added method for returning named entities from paragraph

--- extractor/ner.py
+++ extractor/ner.py
@@ -6,41 +6,33 @@ Created on Feb 18, 2012
 
 import nltk
 
-def extract_entity_names(t):
+def _get_tree_text(tree):
+   pass 
+
+def extract_entity_names(t, ent_type="GPE"):
     entity_names = []
     
-    if hasattr(t, 'node') and t.node:
-        if t.node == 'NE':
+    if isinstance(t, nltk.tree.Tree):
+        if t.node == ent_type:
             entity_names.append(' '.join([child[0] for child in t]))
         else:
             for child in t:
-                entity_names.extend(extract_entity_names(child))
+                entity_names.extend(extract_entity_names(child, ent_type=ent_type))
                 
     return entity_names
 
-def ne_preprocess(paragraph):
-#    sentences = nltk.sent_tokenize(paragraph)
-#    tokenized_sentences = [nltk.word_tokenize(sentence) for sentence in sentences]
-#    tagged_sentences = [nltk.pos_tag(sentence) for sentence in tokenized_sentences]
-#    chunked_sentences = nltk.batch_ne_chunk(tagged_sentences)
-
+def get_named_entities(paragraph, ent_type):
     sentences = nltk.sent_tokenize(paragraph)
     tokenized_sentences = [nltk.word_tokenize(sentence) for sentence in sentences]
     tagged_sentences = [nltk.pos_tag(sentence) for sentence in tokenized_sentences]
-    chunked_sentences = nltk.batch_ne_chunk(tagged_sentences, binary=True)
+    chunked_sentences = nltk.batch_ne_chunk(tagged_sentences)
 
     entity_names = []
     for tree in chunked_sentences:
-        # Print results per sentence
-        # print extract_entity_names(tree)
-        entity_names.extend(extract_entity_names(tree))
+        entity_names.extend(extract_entity_names(tree, ent_type))
     
-    # Print all entity names
-    #print entity_names
-    # Print unique entity names
-    print set(entity_names)
-    # Print entity tree
-    chunked_sentences = nltk.batch_ne_chunk(tagged_sentences)
-    print(chunked_sentences)
-    
-    
+    keywords = ''
+    for ent in entity_names:
+        keywords = keywords + ' ' + ent
+
+    return keywords
